96bc460ece1859dcd9de6a73aae6b1f26fac2b23
“creativeeps23” <creativeps23@gmail.com>
2025-10-22 03:46:26 +0300

    “V1”

---
+++ army_workshop/army_workshop/doctype/workshop_items/workshop_items.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Creative Programming Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Workshop Items', {
+	// refresh: function(frm) {
+
+	// }
+});
