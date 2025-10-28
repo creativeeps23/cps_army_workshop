b907288ca44a2bd2391287b838fa344f8fe79b5d
“creativeeps23” <creativeps23@gmail.com>
2025-10-28 13:01:34 +0300

    “V1”

---
+++ army_workshop/army_workshop/doctype/army_subunit/army_subunit.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Creative Programming Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Army Subunit', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ army_workshop/army_workshop/doctype/manufacture/manufacture.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Creative Programming Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Manufacture', {
+	// refresh: function(frm) {
+
+	// }
+});
