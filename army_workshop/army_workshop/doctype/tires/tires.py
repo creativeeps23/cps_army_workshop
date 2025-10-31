a7af27e1493baefe04ea87764688fc1bbbe55e6f
“creativeeps23” <creativeps23@gmail.com>
2025-10-31 20:49:19 +0200

    “V1”

---
+++ army_workshop/army_workshop/doctype/tires/tires.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Creative Programming Solutions and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Tires(Document):
+	pass
